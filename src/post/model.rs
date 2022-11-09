b316b814ffdba8df77a0fce1e38b995ab4fb38ce
Stanley Winata <winatastanley355@gmail.com>
2022-11-09 16:17:10 +0700

    Some scratch

--- src/post/model.rs
+++ src/post/model.rs
@@ -1,9 +1,12 @@
 use super::req::CreatePostPayload;
 use crate::db::PgPool;
-use crate::schema::posts;
+use crate::schema::{channels, posts};
 
 use actix_web::web;
-use diesel::{BoolExpressionMethods, ExpressionMethods, QueryDsl, QueryResult, RunQueryDsl};
+use diesel::{
+    BoolExpressionMethods, ExpressionMethods, JoinOnDsl, NullableExpressionMethods, QueryDsl,
+    QueryResult, RunQueryDsl,
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Queryable, Debug, Clone, Deserialize, Serialize)]
@@ -18,6 +21,14 @@ pub struct Post {
     post_type: String,
     is_free: bool,
 }
+
+// #[derive(Queryable, Debug, Clone, Deserialize, Serialize)]
+// pub struct Poster {
+//     id: i32,
+//     channels_id: i32,
+//     channels_slug: String,
+//     channel_name: String,
+// }
 
 impl Post {
     pub fn create(
@@ -55,6 +66,7 @@ impl Post {
         posts::table
             .filter(posts::is_free.eq(true))
             .order(posts::created_at.desc())
+            .limit(20)
             .get_results(conn)
     }
 
@@ -63,6 +75,25 @@ impl Post {
         subscriptions: Vec<i32>,
     ) -> QueryResult<Vec<Post>> {
         let conn = &pool.get().unwrap();
+
+        // let selection = (
+        //     posts::id,
+        //     posts::channels_id,
+        //     posts::channels_slug,
+        //     channels::channel_name,
+        // );
+
+        // posts::table
+        //     .filter(
+        //         posts::channels_id
+        //             .eq_any(subscriptions)
+        //             .or(posts::is_free.eq(true)),
+        //     )
+        //     .order(posts::created_at.desc())
+        //     .left_join(channels::table.on(posts::channels_id.eq(channels::id)))
+        //     .select(selection.nullable())
+        //     .get_results(conn)
+
 
         posts::table
             .filter(
